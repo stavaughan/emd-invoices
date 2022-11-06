f390fab9d5153a5ab503ac7dcd8cce9f1b0a90ef
Stephen Vaughan <20270186+stavaughan@users.noreply.github.com>
2022-11-06 15:00:46 -0600

    Create index.js

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,25 @@
+import HomePage from './Home/Home';
+import Invoices from './Invoices/Invoices';
+import Customers from './Customers/Customers';
+import Businesses from './Businesses/Businesses';
+import Products from './Products/Products';
+import UserSettings from './Settings/Settings';
+import UserProfile from './Profile/Profile';
+import NotFound from './NotFound/NotFound';
+import Unauthorized from './Unauthorized';
+import Admin from './Admin/Admin';
+
+const Pages = {
+	HomePage,
+    Invoices,
+	Customers,
+	Businesses,
+	Products,
+    Admin,
+    UserProfile,
+    UserSettings,
+    NotFound,
+    Unauthorized
+};
+
+export default Pages;
